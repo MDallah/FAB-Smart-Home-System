06fc0f95bbe49f4f83bdb336d20256b21e9177f7
md <md@fabmation.de>
2021-08-09 12:19:11 +0200

    Add DHT sensor basic script

--- ESP32/FAB-Smart-Home-System/FAB-Smart-Home-System.ino
+++ ESP32/FAB-Smart-Home-System/FAB-Smart-Home-System.ino
@@ -2,6 +2,7 @@
 #include "WiFi.h"
 #include "ESPAsyncWebServer.h"
 #include "SPIFFS.h"
+#include "DHT.h"
 
 // WiFi SSID & Password
 const char* ssid = "FAB-Smart-Home";
@@ -16,9 +17,23 @@ IPAddress gateway(192, 168, 1, 1);
 
 IPAddress subnet(255, 255, 255, 0);
 
+// Digital pin connected to the DHT sensor
+#define DHTPIN 33 // musst be a output digital pin
+
+// Uncomment whatever type you're using!
+#define DHTTYPE DHT11   // DHT 11
+//#define DHTTYPE DHT22 // DHT 22  (AM2302), AM2321
+//#define DHTTYPE DHT21   // DHT 21 (AM2301)
+
+// Initialize DHT sensor.
+DHT dht(DHTPIN, DHTTYPE);
+
+
 // Set LED GPIO
 const int ledPin1 = 2;
 const int ledPin2 = 4;
+
+
 
 // Stores LED state
 String ledState1;
@@ -79,11 +94,45 @@ void setup() {
   WiFi.softAPConfig(local_ip, gateway, subnet);
   delay(100);
 
+  dht.begin();
   setupServer();
 }
 
 void loop() {
+  // Wait a few seconds between measurements.
+  delay(2000);
 
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(true);
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f))
+  {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+
+  // Compute heat index in Fahrenheit (the default)
+  float hif = dht.computeHeatIndex(f, h);
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(t);
+  Serial.print(F("°C "));
+  Serial.print(f);
+  Serial.print(F("°F  Heat index: "));
+  Serial.print(hic);
+  Serial.print(F("°C "));
+  Serial.print(hif);
+  Serial.println(F("°F"));
 }
 
 // Wait for WiFi connection, and, if not connected, reboot
--- README.md
+++ README.md
@@ -19,4 +19,6 @@ https://randomnerdtutorials.com/esp32-useful-wi-fi-functions-arduino/           
 https://randomnerdtutorials.com/esp32-send-email-smtp-server-arduino-ide/                   // SMTP- Send an Email!
 https://randomnerdtutorials.com/esp32-web-server-spiffs-spi-flash-file-system/              // Web Server & API Handeln
 https://techoverflow.net/2021/01/21/how-to-fix-esp32-not-connecting-to-the-wifi-network/    // Resolve Connection Problems
+https://randomnerdtutorials.com/esp32-dht11-dht22-temperature-humidity-sensor-arduino-ide/  // Temperature and Humidity Sensor
+https://randomnerdtutorials.com/solved-dht11-dht22-failed-to-read-from-dht-sensor/          // Resolve Failed to read (DHT sensor)
 
